40dd78c0da5922fd61385197de1d4afe0e93cb4a
Agnieszka Jablonska <jablonska96@onet.pl>
2025-04-10 19:25:00 +0200

    Add authors QuerySelectMultipleField

--- app/forms.py
+++ app/forms.py
@@ -1,10 +1,20 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, IntegerField
+
+from wtforms import StringField, IntegerField, SubmitField
+from wtforms_sqlalchemy.orm import QuerySelectMultipleField
 from wtforms.validators import DataRequired
+
+from app.models import Author, Movie
+from app import app
+
+def all_authors():
+    with app.app_context():
+        return Author.query.all()
 
 class MovieForm(FlaskForm):
     title = StringField('title', validators=[DataRequired()])
     year = IntegerField('year', validators=[DataRequired()])
+    authors = QuerySelectMultipleField('authors', query_factory=all_authors, get_label="name", validators=[DataRequired()])
 
 class AuthorForm(FlaskForm):
     name = StringField('name', validators=[DataRequired()])
